11139bfdfe97eaf8e8ceb71f35dc894786ded2e6
Atkinss <jordanatkins@live.com>
2024-06-11 21:10:01 -0600

    add: nav bar layout, basic css

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -1,20 +1,20 @@
+import './Nav.scss'
+
 const Nav = () => {
   return (
     <div className="navbar-container">
-      <div className="navbar-wrapper">
-        <div className="navbar-components">
-          <h3>Class</h3>
-          <h3>Equipment</h3>
-          <h3>Feats</h3>
-          <h3>Features</h3>
-          <h3>Monsters</h3>
-          <h3>Races</h3>
-          <h3>Rules</h3>
-          <h3>Spells</h3>
-          <h3>Subclasses</h3>
-          <h3>Subraces</h3>
-          <h3>Traits</h3>
-        </div>
+      <div className="navbar-components">
+        <h3>Classes</h3>
+        <h3>Equipment</h3>
+        <h3>Feats</h3>
+        <h3>Features</h3>
+        <h3>Monsters</h3>
+        <h3>Races</h3>
+        <h3>Rules</h3>
+        <h3>Spells</h3>
+        <h3>Subclasses</h3>
+        <h3>Subraces</h3>
+        <h3>Traits</h3>
       </div>
     </div>
   )
