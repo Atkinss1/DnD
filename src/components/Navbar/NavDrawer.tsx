0d6c33946a53472a94d934cbecc05fb124c6daba
Atkinss <jordanatkins@live.com>
2024-06-28 17:25:44 -0600

    add: drawer styling

--- src/components/Navbar/NavDrawer.tsx
+++ src/components/Navbar/NavDrawer.tsx
@@ -3,13 +3,11 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 import { useCategories } from '../../hooks/useCategories';
 
 export default function NavDrawer() {
@@ -21,26 +19,13 @@ export default function NavDrawer() {
   };
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+    <Box sx={{ width: 250, background: '#930C10', color: 'white' }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        {Object.keys(topics).map((text, index) => (
+        {Object.keys(topics).map((text) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                 <ArrowCircleRightIcon />
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
