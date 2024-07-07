a867af64880cc5d159ba2f8093bf268175b20cb8
Atkinss <jordanatkins@live.com>
2024-07-07 13:05:51 -0600

    add: topicApi, topicData to reducer/global state

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -19,7 +19,12 @@ export interface SingleTopicData {
 
 // Category Interfaces
 
+export interface BaseCharacterInterface extends TopicCardProps {
+  index: string;
+}
+
 export type BaseCategoryResults = BaseCharacterInterface[];
+
 
 export interface Categories {
   [key: string]: string;
@@ -36,17 +41,27 @@ export type CategoryState = {
   categories: BaseCategoryResults;
   categoryAPI: string;
   topics: Topics;
+  topicAPI: string;
+  topicData: {
+    [key: string]: SingleTopicData;
+  };
   loadingCategories: boolean;
   loadingTopics: boolean;
   error: string | null;
   fetchCategoryData?: (api: string) => Promise<void>;
+  fetchTopicData?: (api: string) => Promise<void>;
+  clearTopicApi?: () => void;
 }
 
 export type CategoryAction =
   | { type: "SET_CATEGORY_API"; payload: string }
+  | { type: "SET_TOPIC_API"; payload: string }
   | { type: "FETCH_CATEGORIES_REQUEST" }
   | { type: "FETCH_CATEGORIES_SUCCESS"; payload: BaseCategoryResults }
   | { type: "FETCH_CATEGORIES_FAILURE"; error: unknown }
   | { type: "FETCH_TOPICS_REQUEST" }
   | { type: "FETCH_TOPICS_SUCCESS"; payload: Topics }
-  | { type: "FETCH_TOPICS_FAILURE"; error: unknown };
+  | { type: "FETCH_TOPICS_FAILURE"; error: unknown }
+  | { type: "FETCH_SINGLE_TOPIC_SUCCESS"; payload: SingleTopicData }
+  | { type: "FETCH_SINGLE_TOPIC_FAILURE"; error: unknown }
+  | { type: "CLEAR_TOPIC_API"; payload: string };
